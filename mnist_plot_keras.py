8621a52da50eb43052f17e4344d5fad38f23a966
nagoyan777 <nagoyan777@hotmail.co.jp>
2017-11-28 23:45:47 +0900

    add early stopping.

--- mnist_plot_keras.py
+++ mnist_plot_keras.py
@@ -1,26 +1,93 @@
 #!/bin/env python
-import os, sys
+import os, sys, dill
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers.core import Dropout
 from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping
+from keras.initializers import TruncatedNormal
+from keras.layers.normalization import BatchNormalization
+from keras import backend as K
+
 
 def main():
+
+    N_train = 20000
+    N_validation = 4000
+
+    X, Y = get_data()
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, train_size=N_train)
+
+    X_train, X_validation, Y_train, Y_validation = train_test_split(
+        X_train, Y_train, test_size=N_validation)
+
     n_in = 784
-    n_hiddens = [200, 200]
-    n_out = 10
+    n_hiddens = [200, 200, 200]
+    n_out = len(Y[0])
     activation = 'relu'
     p_keep = 0.5
 
     model = Sequential()
-    for i, input_dim in enumerate([n_in] + n_hiddens)[:-1]):
-        model.add(Dense(n_hiddens[i], input_dim=input_dim))
+    for i, input_dim in enumerate(([n_in] + n_hiddens)[:-1]):
+        model.add(Dense(n_hiddens[i], input_dim=input_dim,
+        kernel_initializer=TruncatedNormal(stddev=0.01)))
+#         kernel_initializer=weight_variable))
+        model.add(BatchNormalization)
         model.add(Activation(activation))
         model.add(Dropout(p_keep))
 
-    model.add(Dense(n_out))
+#    model.add(Dense(n_out), kernel_initializer=weight_variable)
+    model.add(Dense(n_out, kernel_initializer=TruncatedNormal(stddev=0.01)))
     model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=SGD(lr=0.01), metrics=['accuracy'])
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
+
+    epochs = 50
+    batch_size = 200
+
+    hist = model.fit(X_train, Y_train, epochs=epochs,
+                     batch_size=batch_size,
+                     validation_data=(X_validation, Y_validation),
+                     callbacks=[early_stopping])
+
+    val_acc = hist.history['val_acc']
+
+    plt.rc('font', family='serif')
+    fig = plt.figure()
+    plt.plot(range(len(val_acc)), val_acc, label='acc', color='black')
+    plt.xlabel('epoch')
+    plt.show()
+    # plt.savefig('mnist_keras.eps')
+
+
+def weight_variable(shape):
+    return K.truncated_normal(shape, stddev=0.01)
+
+
+def get_data(filename='mnist.pickle', N=None):
+    if os.path.exists(filename):
+        with open(filename, 'rb') as f:
+            mnist = dill.load(f)
+    else:
+        mnist = datasets.fetch_mldata('MNIST original', data_home='.')
+        with open(filename, 'wb') as g:
+            dill.dump(mnist, g)
+
+    n = len(mnist.data)
+    indices = np.random.permutation(range(n))[:N]
+    X = mnist.data[indices]
+    y = mnist.target[indices]
+    Y = np.eye(10)[y.astype(int)]
+
+    return X, Y
 
 if __name__ == '__main__':
     main()
--- mnist_plot_tensorflow.py
+++ mnist_plot_tensorflow.py
@@ -1,10 +1,11 @@
 #!/bin/env python
 import os, sys, dill
 import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
 def main():
@@ -14,16 +15,19 @@ def main():
     N_validation = 4000
 
     X, Y = get_data()
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=N_train)
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, train_size=N_train)
 
-    X_train, X_validation, Y_train, Y_validation = train_test_split(X_train, Y_train, test_size=N_validation)
+    X_train, X_validation, Y_train, Y_validation = train_test_split(
+        X_train, Y_train, test_size=N_validation)
 
     n_in = 784
     n_hiddens = [200, 200, 200]
     n_out = 10
     model = DNN(n_in, n_hiddens, n_out)
 
-    history = model.fit(X_train, Y_train, X_validation, Y_validation, epochs=50, batch_size=200, p_keep=0.5)
+    history = model.fit(X_train, Y_train, X_validation,
+                        Y_validation, epochs=50, batch_size=200, p_keep=0.5)
 
     print(history['loss'], history['accuracy'])
 
@@ -50,6 +54,7 @@ def get_data(filename='mnist.pickle', N=None):
 
 
 class DNN(object):
+
     def __init__(self, n_in, n_hiddens, n_out):
         self.n_in = n_in
         self.n_hiddens = n_hiddens
@@ -61,7 +66,8 @@ class DNN(object):
         self._t = None
         self._keep_prob = None
         self._sess = None
-        self._history = {'accuracy':[], 'loss':[], 'val_loss':[], 'val_acc':[]}
+        self._history = {'accuracy': [], 'loss': [],
+                         'val_loss': [], 'val_acc': []}
 
     def weight_variable(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.01)
@@ -84,24 +90,26 @@ class DNN(object):
             self.weights.append(self.weight_variable([input_dim, n_hidden]))
             self.biases.append(self.bias_variable([n_hidden]))
 
-            h = tf.nn.relu(tf.matmul(input, self.weights[-1]) + self.biases[-1])
+            h = tf.nn.relu(
+                tf.matmul(input, self.weights[-1]) + self.biases[-1])
             output = tf.nn.dropout(h, keep_prob)
 
-        self.weights.append(self.weight_variable([self.n_hiddens[-1], self.n_out]))
+        self.weights.append(self.weight_variable(
+            [self.n_hiddens[-1], self.n_out]))
         self.biases.append(self.bias_variable([self.n_out]))
 
-        y = tf.nn.softmax(tf.matmul(output, self.weights[-1]) + self.biases[-1])
+        y = tf.nn.softmax(
+            tf.matmul(output, self.weights[-1]) + self.biases[-1])
 
         return y
-
 
     def loss(self, y, t):
         '''define the error function'''
         cross_entropy = tf.reduce_mean(
-                        -tf.reduce_sum(t * tf.log(tf.clip_by_value(y, 1e-10, 1.0)), reduction_indices=[1])
-                        )
+            -tf.reduce_sum(t * tf.log(tf.clip_by_value(y,
+                                                       1e-10, 1.0)), reduction_indices=[1])
+        )
         return cross_entropy
-
 
     def training(self, loss):
         '''define the learning algolithm'''
@@ -115,8 +123,8 @@ class DNN(object):
         return accuracy
 
     def fit(self, X_train, Y_train,
-                X_validation=None, Y_validation=None,
-                epochs=100, batch_size=100, p_keep=0.5, verbose=1):
+            X_validation=None, Y_validation=None,
+            epochs=100, batch_size=100, p_keep=0.5, verbose=1):
         x = tf.placeholder(tf.float32, shape=[None, self.n_in])
         t = tf.placeholder(tf.float32, shape=[None, self.n_out])
         keep_prob = tf.placeholder(tf.float32)
@@ -138,7 +146,8 @@ class DNN(object):
 
         N_train = len(X_train)
         n_batches = N_train // batch_size
-        history = {'val_loss':[], 'val_acc':[]}
+        history = {'val_loss': [], 'val_acc': []}
+        early_stopping = EarlyStopping(patience=10, verbose=1)
         for epoch in range(epochs):
             X_, Y_ = shuffle(X_train, Y_train)
 
@@ -146,31 +155,48 @@ class DNN(object):
                 start = i * batch_size
                 end = start + batch_size
 
-                sess.run(train_step, feed_dict={x: X_[start:end], t: Y_[start:end], keep_prob: p_keep})
+                sess.run(train_step, feed_dict={
+                         x: X_[start:end], t: Y_[start:end], keep_prob: p_keep})
 
-            if not X_validation is None:
-                val_loss = loss.eval(session=sess, feed_dict={x: X_validation, t: Y_validation, keep_prob: 1.0})
-                val_acc = accuracy.eval(session=sess, feed_dict={x: X_validation, t: Y_validation, keep_prob: 1.0})
 
-                history['val_loss'].append(val_loss)
-                history['val_acc'].append(val_acc)
-            loss_ = loss.eval(session=sess, feed_dict={x: X_train, t: Y_train, keep_prob:1.0})
-            accuracy_ = accuracy.eval(session=sess, feed_dict={x:X_train, t:Y_train, keep_prob: 1.0})
+            loss_ = loss.eval(session=sess, feed_dict={
+                              x: X_train, t: Y_train, keep_prob: 1.0})
+            accuracy_ = accuracy.eval(session=sess, feed_dict={
+                                      x: X_train, t: Y_train, keep_prob: 1.0})
 
             self._history['loss'].append(loss_)
             self._history['accuracy'].append(accuracy_)
-            self._history['val_loss'].extend(history['val_loss'])
-            self._history['val_acc'].extend(history['val_acc'])
+
             if verbose:
-                print('epoch:', epoch, ' loss:', loss_, ' accuracy:', accuracy_)
+                print('epoch:', epoch, ' loss:',
+                      loss_, ' accuracy:', accuracy_)
+
+            if not X_validation is None:
+                val_loss = loss.eval(session=sess, feed_dict={
+                                     x: X_validation, t: Y_validation, keep_prob: 1.0})
+                val_acc = accuracy.eval(session=sess, feed_dict={
+                                        x: X_validation, t: Y_validation, keep_prob: 1.0})
+
+                history['val_loss'].append(val_loss)
+                history['val_acc'].append(val_acc)
+
+                self._history['val_loss'].extend(history['val_loss'])
+                self._history['val_acc'].extend(history['val_acc'])
+
+
+                if early_stopping.validate(val_loss):
+                    break
+
 
         if not X_validation is None:
             plt.rc('font', family='serif')
             fig = plt.figure()
             ax_acc = fig.add_subplot(111)
-            ax_acc.plot(range(epochs), history['val_acc'], label='acc', color='black')
+            ax_acc.plot(range(epochs), history[
+                        'val_acc'], label='acc', color='black')
             ax_loss = ax_acc.twinx()
-            ax_loss.plot(range(epochs), history['val_loss'], label='loss', color='gray')
+            ax_loss.plot(range(epochs), history[
+                         'val_loss'], label='loss', color='gray')
             plt.xlabel('epochs')
 #            plt.ylabel('validation loss')
             plt.legend()
@@ -180,6 +206,29 @@ class DNN(object):
 
     def evaluate(self, X_test, Y_test):
         return self.accuracy.eval(session=self._sess, feed_dict={self._x: X_test, self._t: Y_test, self._keep_prob: 1.0})
+
+
+class EarlyStopping():
+    def __init__(self, patience=0, verbose=0):
+        self._step = 0
+        self._loss = float('inf')
+        self.patience = patience
+        self.verbose = verbose
+
+    def validate(self, loss):
+        if self._loss < loss:
+            self._step += 1
+            if self._step > self.patience:
+                if self.verbose:
+                    print('early stopping')
+                return True
+        else:
+            self._step = 0
+            self._loss = loss
+
+        return False
+
+
 
 if __name__ == '__main__':
     main()
